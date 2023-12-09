9b51b3f92eff776c39925efd14beb61ae61ec46f
saidt <saeid@metropolia.fi>
2023-12-10 00:51:25 +0200

    moduuli

--- python tehtavat/moduuli 13/tehtävä 2.py
+++ python tehtavat/moduuli 13/tehtävä 2.py
@@ -1,1 +1,35 @@
+from flask import Flask, jsonify
+import pymysql
 
+app = Flask(__name__)
+
+# Function to query the database
+def get_airport_info(icao):
+    connection = pymysql.connect(
+        host='localhost',
+        user='root',
+        password='saeidt',
+        database='',
+        cursorclass=pymysql.cursors.DictCursor
+    )
+
+    try:
+        with connection.cursor() as cursor:
+            sql = "SELECT ICAO, Name, Location FROM airports WHERE ICAO = %s"
+            cursor.execute(sql, (icao,))
+            result = cursor.fetchone()
+            return result
+    finally:
+        connection.close()
+
+# API endpoint to get airport information
+@app.route('/airport/<icao>', methods=['GET'])
+def airport_info(icao):
+    airport = get_airport_info(icao)
+    if airport:
+        return jsonify(airport)
+    else:
+        return jsonify({"error": "Airport not found"}), 404
+
+if __name__ == '__main__':
+    app.run(debug=True)
