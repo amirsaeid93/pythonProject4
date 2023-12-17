9b2673770cf08e4a4739bfc484669985091b43eb
saidt <saeid@metropolia.fi>
2023-12-17 16:55:47 +0200

    Javascript moduuli 3

--- java_moduuli_3/tehtava_3/3.js
+++ java_moduuli_3/tehtava_3/3.js
@@ -1,16 +1,17 @@
 'use strict';
+
 const names = ['John', 'Paul', 'Jones'];
 
-// Get the element with id="target"
+
 const targetElement = document.getElementById('target');
 
-// Create a string to hold the HTML content
+
 let htmlContent = '';
 
-// Loop through the names array and create <li> elements
+
 for (let i = 0; i < names.length; i++) {
 htmlContent += `<li>${names[i]}</li>`;
 }
 
-// Set the innerHTML of the target element to the generated HTML content
+
 targetElement.innerHTML = htmlContent;
