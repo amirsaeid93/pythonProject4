765f1e13690cf29b0e463a94a3906a88d9ef5a97
saidt <saeid@metropolia.fi>
2023-12-17 21:42:08 +0200

    Javascript moduuli 3

--- java_moduuli_4/2.js
+++ java_moduuli_4/2.js
@@ -1,5 +1,8 @@
 'use strict'
+
 const form = document.getElementById('searchForm');
+
+const resultsContainer = document.getElementById('results');
 
 form.addEventListener('submit', async function(event) {
 event.preventDefault();
@@ -7,18 +10,41 @@ event.preventDefault();
 
 const userInput = document.getElementById('query').value;
 
-// Make a request to the TVMaze API
+
 const response = await fetch(`https://api.tvmaze.com/search/shows?q=${userInput}`);
 const data = await response.json();
 
-if (data && data.length > 0) {
-const show = data[0].show;
-console.log('Show Name:', show.name);
-console.log('Show ID:', show.id);
-console.log('Show Summary:', show.summary);
-console.log('Show URL:', show.url);
+resultsContainer.innerHTML = '';
 
+if (data && data.length > 0) {
+
+const show = data[0].show;
+
+const article = document.createElement('article');
+const heading = document.createElement('h2');
+heading.textContent = show.name;
+
+const link = document.createElement('a');
+link.href = show.url;
+link.textContent = 'Show Details';
+link.target = '_blank';
+
+const image = document.createElement('img');
+image.src = show.image?.medium || 'https://via.placeholder.com/210x295';
+image.alt = show.name;
+
+const summary = document.createElement('div');
+summary.innerHTML = show.summary;
+
+
+article.appendChild(heading);
+article.appendChild(link);
+article.appendChild(image);
+article.appendChild(summary);
+
+
+resultsContainer.appendChild(article);
 } else {
-console.log('Sorry, no information was found regarding the inputed TV show.');
+resultsContainer.textContent = 'No information found about the searched TV show.';
 }
 });
